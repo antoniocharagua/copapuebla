8ded6882437e6967d65f721bd5b8b14854ca2fc3
antoniocharagua <antonio.charagua@gmail.com>
2015-11-05 19:47:08 -0600

    Desarrollo de la interfaz para agregar un equipo al torneo, inestable

--- back/src/main/java/com/ficufi/copapuebla/back/service/dto/EquipoDto.java
+++ back/src/main/java/com/ficufi/copapuebla/back/service/dto/EquipoDto.java
@@ -1,12 +1,14 @@
 package com.ficufi.copapuebla.back.service.dto;
 
 import com.ficufi.copapuebla.persistence.entities.Equipo;
+import java.util.Objects;
 
 /**
  * Data Transfer Object para un equipo
  * @author Antonio Francisco Alonso Valerdi
  */
 public class EquipoDto {
+public class EquipoDto implements Comparable<EquipoDto>{
     
     private int id;
     private String nombre;
@@ -49,6 +51,41 @@ public class EquipoDto {
         StringBuilder string = new StringBuilder();
         string.append(" id:").append(id).append(" nombre:").append(nombre).append(" categoria:").append(categoria);
         return string.toString();
+    }
+
+    @Override
+    public int compareTo(EquipoDto equipoDto) {
+        return id - equipoDto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + this.id;
+        hash = 37 * hash + Objects.hashCode(this.nombre);
+        hash = 37 * hash + Objects.hashCode(this.categoria);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final EquipoDto other = (EquipoDto) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.categoria, other.categoria)) {
+            return false;
+        }
+        return true;
     }
 
 }
